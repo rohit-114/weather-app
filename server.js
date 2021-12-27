66df7b0d72c1cbe2edd0f6c454496a712f2ae403
Rohit <shinderohit004@gmail.com>
2021-12-27 18:41:24 +0530

    change server.js

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.get("/api/:latitude/:longitude", async (req, res) => {
   }
 });
 
-app.get("*", (req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname + "/client/build/index.html"));
 });
 
